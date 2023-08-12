8a2725bce8925ae0db6f12b6218c7c2728944a37
brian <2539907687@qq.com>
2023-08-12 18:47:29 +0800

    add first animation

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,30 @@
+"use client";
 import Image from "next/image";
-import Me from "@/public/IMG_0214.png";
+// import Me from "@/public/IMG_0214.png";
+import { motion, Variants } from "framer-motion";
+import * as React from "react";
 
-export default function Home() {
+const contaioner = {
+  hidden: { opacity: 1, scale: 0 },
+  visible: {
+    opacity: 1,
+    scale: 2,
+    transition: {
+      delayChildren: 0.3,
+      staggerChildren: 0.2,
+    },
+  },
+};
+
+const item = {
+  hidden: { x: -50, opacity: 0 },
+  visible: {
+    x: 0,
+    opacity: 1,
+  },
+};
+
+export default function Home(props: any) {
   return (
     <div className="divide-y divide-gray-100 dark:divide-gray-700">
       <div className="space-y-2 pt-6 pb-8 md:space-y-5">
@@ -9,19 +32,29 @@ export default function Home() {
           Home
         </h1>
       </div>
+
       <div className="items-center space-y-2 xl:grid xl:grid-cols-3 xl:gap-x-8 xl:space-y-0">
         <div className="flex flex-col items-center pt-8">
-          <Image
-            alt="Picture of Brian Chung"
-            src={Me}
-            width={500}
-            height={500}
-            className="h-58 w-48 rounded-full object-cover object-top"
-          />
+          <motion.div
+            initial={{ opacity: 0, scale: 0.8 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ ease: "easeOut", duration: 0.5 }}
+          >
+            <Image
+              alt="Picture of Brian Chung"
+              src="/IMG_0214.png"
+              width={500}
+              height={500}
+              className="h-58 w-48 rounded-full object-cover object-top"
+            />
+          </motion.div>
+
           <h3 className="pt-4 pb-2 text-2xl font-bold leading-8 tracking-tight">
             Bulahe Chung
           </h3>
+
           <p className="text-gray-500 dark:text-gray-300 text-center"></p>
+
           <div className="flex space-x-5 pt-6">
             {/* github帳戶 */}
             <a href="https://github.com/bulahe" target="_blank">
@@ -33,28 +66,6 @@ export default function Home() {
                 <path d="M511.6 76.3C264.3 76.2 64 276.4 64 523.5 64 718.9 189.3 885 363.8 946c23.5 5.9 19.9-10.8 19.9-22.2v-77.5c-135.7 15.9-141.2-73.9-150.3-88.9C215 726 171.5 718 184.5 703c30.9-15.9 62.4 4 98.9 57.9 26.4 39.1 77.9 32.5 104 26 5.7-23.5 17.9-44.5 34.7-60.8-140.6-25.2-199.2-111-199.2-213 0-49.5 16.3-95 48.3-131.7-20.4-60.5 1.9-112.3 4.9-120 58.1-5.2 118.5 41.6 123.2 45.3 33-8.9 70.7-13.6 112.9-13.6 42.4 0 80.2 4.9 113.5 13.9 11.3-8.6 67.3-48.8 121.3-43.9 2.9 7.7 24.7 58.3 5.5 118 32.4 36.8 48.9 82.7 48.9 132.3 0 102.2-59 188.1-200 212.9a127.5 127.5 0 0138.1 91v112.5c.8 9 0 17.9 15 17.9 177.1-59.7 304.6-227 304.6-424.1 0-247.2-200.4-447.3-447.5-447.3z" />
               </svg>
             </a>
-            {/* 
-            
-            <a href="" target="_blank">
-              <svg
-                viewBox="0 0 16 16"
-                fill="currentColor"
-                className="w-8 h-8 text-teal-500 hover:text-teal-600"
-              >
-                <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 01.016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z" />
-              </svg>
-            </a>
-            
-            <a href="a" target="_blank">
-              <svg
-                viewBox="0 0 1024 1024"
-                fill="currentColor"
-                className="w-8 h-8 text-teal-500 hover:text-teal-600"
-              >
-                <path d="M928 160H96c-17.7 0-32 14.3-32 32v640c0 17.7 14.3 32 32 32h832c17.7 0 32-14.3 32-32V192c0-17.7-14.3-32-32-32zm-80.8 108.9L531.7 514.4c-7.8 6.1-18.7 6.1-26.5 0L189.6 268.9A7.2 7.2 0 01194 256h648.8a7.2 7.2 0 014.4 12.9z" />
-              </svg>
-            </a>
-             */}
 
             {/* Twitter帳戶 */}
             <a href="https://twitter.com/w4c5a" target="_blank">
